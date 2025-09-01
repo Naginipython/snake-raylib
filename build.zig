0c02762d6f6e28fc858e69692b3e3cc6531a3ba0
Naginipython <naginipython332@gmail.com>
2025-09-01 15:27:04 -0500

    Updated Zig to 0.15.1 (Test doesn't work)

--- build.zig
+++ build.zig
@@ -12,129 +12,146 @@ fn linker(exe: *std.Build.Step.Compile, files: []const []const u8, b: *std.Build
     exe.linkLibCpp();
 
     // Libs
-    if (target.query.isNativeOs() and target.result.os.tag == .windows) {
-        // Solution 1
-        const sdl_dep = b.dependency("raylib-zig", .{ // Add libs as needed
-            .target = target,
-        });
-        exe.linkLibrary(sdl_dep.artifact("raylib"));
+    const sdl_dep = b.dependency("raylib_zig", .{
+        .target = target,
+    });
+    exe.linkLibrary(sdl_dep.artifact("raylib"));
 
-        // Solution 2 -WIP-
-        // This worked in the past?
-        // exe.addIncludePath(b.path("lib/SDL2/include/"));
-        // exe.addLibraryPath(b.path("lib/SDL2/lib/x64/SDL2.lib"));
-        // b.installBinFile("lib/SDL2/lib/x64/SDL2.dll", "SDL2.dll");
-        // exe.linkSystemLibrary("SDL2");
-    } else {
-        exe.linkSystemLibrary("raylib");
-    }
+    // Other lib solutions
+    // if (target.query.isNativeOs() and target.result.os.tag == .windows) {
+        // Solution 2 (make sure you place all files in the directory, not just the lib files (unless you know what you're doing))
+        // exe.addLibraryPath(b.path("lib/SDL3-3.1.6/lib/x64/"));
+        // exe.linkSystemLibrary("SDL3");
+        // b.installBinFile("lib/SDL3-3.1.6/lib/x64/SDL3.dll", "SDL3.dll");
+
+        // Solution 3: Dynamic Library? -WIP-
+        // b.installBinFile("lib/SDL3-3.1.6/lib/x64/SDL3.dll", "SDL3.dll");
+    
 }
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
 
-    const files = try findFiles("src", &[_][]const u8{});
-    std.debug.print("Files built:\n{s}\n", .{files});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
-    const exe = b.addExecutable(.{ .name = PROGRAM_NAME, .target = target });
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+
+    const files = try findFiles("src", &.{}, &arena);
+    std.debug.print("Files to build: [\n", .{});
+    for (files) |f| {
+        std.debug.print("\t{s}\n", .{f}); // f is []const u8, this is correct
+    }
+    std.debug.print("]\n", .{});
+    
+    const exe = b.addExecutable(.{
+        .name = PROGRAM_NAME,
+        .root_module = b.createModule(.{
+            .target = target,
+        }),
+    });
     linker(exe, files, b, target);
 
     b.installArtifact(exe);
 
     // ------ Run ------
+    const run_step = b.step("run", "Run the app");
+
     const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
     run_cmd.step.dependOn(b.getInstallStep());
 
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
     // ------ Tests ------
-    var test_dir_exists = false;
-    _ = std.fs.cwd().statFile("tests") catch |err| {
-        // std.debug.print("{?}\n", .{err});
-        if (err == error.IsDir) {
-            test_dir_exists = true;
-        }
-    };
-    
-    if (test_dir_exists) {
-        const test_files = try findFiles("tests", &[_][]const u8{"main.cpp"});
-        std.debug.print("Test Files built:\n{s}\n", .{test_files});
+    // if (try testDirExists()) {
+    //     const test_files1 = try findFiles("tests", &[_][]const u8{}, &arena);
+    //     const test_files2 = try findFiles("src", &[_][]const u8{"main.cpp"}, &arena);
 
-        // combine with `files`, except src/main.c or src/main.cpp
-        const tests_name = PROGRAM_NAME ++ "_tests";
-        const tests_exe = b.addExecutable(.{ .name = tests_name, .target = target });
+    //     // Combine the test files with the src files
+    //     const test_files = try combineArrays(test_files1, test_files2, &arena);
+    //     std.debug.print("Test files to build: [\n", .{});
+    //     for (test_files) |f| {
+    //         std.debug.print("\t{s}\n", .{f}); // f is []const u8, this is correct
+    //     }
+    //     std.debug.print("]\n", .{});
 
-        linker(tests_exe, test_files, b, target);
-        const googletest_dep = b.dependency("googletest", .{
-            .target = target,
-            // .optimize = b.standardOptimizeOption(.{}),
-        });
-        tests_exe.linkLibrary(googletest_dep.artifact("gtest"));
+    //     // combine with `files`, except src/main.c or src/main.cpp
+    //     const tests_name = PROGRAM_NAME ++ "_tests";
+    //     const tests_exe = b.addExecutable(.{ .name = tests_name, .root_module = b.createModule(.{ .target = target }) });
 
-        b.installArtifact(tests_exe);
+    //     linker(tests_exe, test_files, b, target);
+    //     const googletest_dep = b.dependency("googletest", .{
+    //         .target = target,
+    //         // .optimize = b.standardOptimizeOption(.{}),
+    //     });
+    //     tests_exe.linkLibrary(googletest_dep.artifact("gtest"));
 
-        const run_tests_exe = b.addRunArtifact(tests_exe);
-        run_tests_exe.step.dependOn(b.getInstallStep());
+    //     b.installArtifact(tests_exe);
 
-        const test_step = b.step("test", "Run unit tests");
-        test_step.dependOn(&run_tests_exe.step);
-    }
+    //     const run_tests_exe = b.addRunArtifact(tests_exe);
+    //     run_tests_exe.step.dependOn(b.getInstallStep());
 
-    // ------ Clean ------
-    // const clean_step = b.step("clean", "Clean the directory");
-    // Windows
-    // clean_step.dependOn(&b.addRemoveDirTree(b.path("zig-out")).step);
-    // clean_step.dependOn(&b.addRemoveDirTree(b.path(".zig-cache")).step);
-    // Linux
-    // clean_step.dependOn(&b.addRemoveDirTree(b.install_path).step);
-    // clean_step.dependOn(&b.addRemoveDirTree(b.pathFromRoot(".zig-cache")).step);
+    //     const test_step = b.step("test", "Run unit tests");
+    //     test_step.dependOn(&run_tests_exe.step);
+    // }
 }
 
-// TODO: Add ignore word
-fn findFiles(src: []const u8, ignore_list: []const []const u8) ![]const []const u8 {
-    var result = std.ArrayList([]const u8).init(std.heap.page_allocator);
-    // todo: error handling for root (for test dir)
+fn testDirExists() !bool {
+    const checkDirs = try std.fs.cwd().openDir(".", .{ .iterate = true });
+    var iter = checkDirs.iterate();
+    while (try iter.next()) |entry| {
+        if (std.mem.eql(u8, entry.name,"tests") and entry.kind == .directory) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn combineArrays(arr1: []const []const u8, arr2: []const []const u8, arena: *std.heap.ArenaAllocator) ![]const []const u8 {
+    const alloc = arena.allocator();
+    var combined: std.ArrayList([]const u8) = .empty;
+    defer combined.deinit(alloc);
+    for (arr1) |item|
+        try combined.append(alloc, item);
+    for (arr2) |item|
+        try combined.append(alloc, item);
+    return try combined.toOwnedSlice(alloc);
+}
+
+fn findFiles(src: []const u8, ignore_list: []const []const u8, arena: *std.heap.ArenaAllocator) ![]const []const u8 {
+    const alloc = arena.allocator();
+
+    var result: std.ArrayList([]const u8) = .empty;
+    defer result.deinit(alloc);
+
     var root = try std.fs.cwd().openDir(src, .{ .iterate = true });
     defer root.close();
 
     var iter = root.iterate();
-    while (try iter.next()) |entry| {
-        // ignore if on ignore list
-        var ignore = false;
+    main: while (try iter.next()) |entry| {
+        // ignore if on ignore list (partial matching)
         for (ignore_list) |item|
-            if (std.mem.indexOf(u8, entry.name, item) != null) {
-                ignore = true;
-                break;
-            };
-        if (ignore) {
-            continue;
-        }
+            if (std.mem.indexOf(u8, entry.name, item) != null)
+                continue :main;
 
-        // Create item
-        var item = std.ArrayList(u8).init(std.heap.page_allocator);
-        try item.appendSlice(src);
-        try item.append('/');
-        try item.appendSlice(entry.name);
         if (entry.kind == .file) {
-            const check_cpp = entry.name[entry.name.len - 4 ..];
-            const check_c = entry.name[entry.name.len - 2 ..];
-            if (std.mem.eql(u8, check_cpp, ".cpp") or std.mem.eql(u8, check_c, ".c")) {
-                const path_u8 = try item.toOwnedSlice();
-                try result.append(path_u8);
+            if (std.mem.endsWith(u8, entry.name, ".c") or
+                std.mem.endsWith(u8, entry.name, ".cpp"))
+            {
+                const path_u8 = try std.fmt.allocPrint(alloc, "{s}/{s}", .{src, entry.name});
+                try result.append(alloc, path_u8);
             }
         }
-        if (entry.kind == .directory) {
-            const dir_u8 = try item.toOwnedSlice();
-            const files = try findFiles(dir_u8, ignore_list);
-            for (files) |f|
-                try result.append(f);
+        // Recusively search for files in directories TODO
+        else if (entry.kind == .directory) {
+            const dir_u8 = try std.fmt.allocPrint(alloc, "{s}/{s}", .{src, entry.name});
+            const files = try findFiles(dir_u8, ignore_list, arena);
+            try result.appendSlice(alloc, files);
         }
     }
-    const res = try result.toOwnedSlice();
-    return res;
+    return try result.toOwnedSlice(alloc);
 }
